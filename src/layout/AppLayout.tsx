217b7e5346599a1f61909f193ad264eb7854d6a9
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 00:04:52 +0100

    Added Header and bg colors

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import Header from '../components/Header';
+
 type Props = {
     disableLayout?: boolean
 }
 const AppLayout = ({ disableLayout = false }: Props) => {
-    return disableLayout ? <Outlet /> : (<>
-        <div>head</div>
+    return disableLayout ? <Outlet /> : (<div className={'bg-bg-light-inner dark:bg-bg-dark-inner flex flex-col w-full'}>
+        <Header visible={true} />
         <Outlet />
         <div>footer</div>
-    </>);
+    </div>);
 }
 
 export default AppLayout;

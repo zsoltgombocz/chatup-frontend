f02207258cf0949710a43f38e932875e2cb5916a
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 20:30:12 +0100

    included footer

--- src/views/SettingsView.tsx
+++ src/views/SettingsView.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from '../components/Footer';
 
 function SettingsView() {
     return (
-        <div>SettingsView</div>
-    );
+        <>
+            <div>SettingsView</div>
+
+            <Footer />
+        </>);
 }
 
 export default SettingsView;

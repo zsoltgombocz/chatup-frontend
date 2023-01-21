9bdced6e7932d321a019af644e1f4c6f5d6d4749
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 18:07:45 +0100

    added route names

--- src/config/headerConfig.ts
+++ src/config/headerConfig.ts
@@ -1,3 +1,14 @@
-export const config = {
-    useNavigationRoutes: ['settings']
+interface HeaderConfigInterface {
+    useNavigationRoutes: string[],
+    routeNames: { [id: string]: string };
+
+}
+
+export const config: HeaderConfigInterface = {
+    useNavigationRoutes: ['settings'],
+    routeNames: {
+        'devlogs': 'Verzióelőzmények',
+        'settings': 'Beállítások',
+        'settings/privacy': 'Adatvédelem'
+    }
 }

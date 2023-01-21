86469998d6fcf8c95fc6f07115e1b81faf826100
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 17:03:27 +0100

    removed unnecessary variables
    moved all static class-es to index.css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,15 +22,12 @@ function Button({ size, style, color = undefined, text, className, linkTo = unde
         'primary': 'w-80 py-4',
         'secondary': 'w-50 px-14 h-10 leading-10',
     }
-    const darkOutlinedStyle = 'dark:text-white dark:border-white hover:dark:text-gray-600 hover:dark:bg-white';
-    const lightOutlinedStyle = 'text-gray-600 border-gray-600 hover:text-white hover:bg-gray-600';
-    const baseStyle = 'text-xl flex justify-center items-center tracking-wide font-cabin rounded-full';
-    const outlinedStyle = classnames('bg-transparent border-solid border', darkOutlinedStyle, lightOutlinedStyle);
+    const outlinedStyle = classnames('outlined', 'outlined-dark');
     const fillColor = color !== undefined ? color.fill : 'bg-pr-blue';
     const hoverColor = color !== undefined ? color.hover : 'hover:bg-pr-blue-light';
     const filledStyle = classnames(fillColor, hoverColor, 'text-white');
     const buttonJSXElement = (
-        <button className={`${baseStyle} ${className} ${style === 'filled' ? filledStyle : outlinedStyle} ${buttonSize[size]}`}>
+        <button className={`btn ${className} ${style === 'filled' ? filledStyle : outlinedStyle} ${buttonSize[size]}`}>
             <span>{text.toUpperCase()}</span>
         </button>
     )

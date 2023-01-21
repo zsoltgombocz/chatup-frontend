cbce294e095b984b29e337093e398f935a21e4a6
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 20:29:12 +0100

    added classname prop and show env status

--- src/atoms/Version.tsx
+++ src/atoms/Version.tsx
@@ -1,9 +1,14 @@
-const Version = () => {
+type Props = {
+    showEnv?: boolean,
+    className?: string
+}
+
+const Version = ({ showEnv = false, className }: Props) => {
     //! TODO: DO NOT READ FROM .ENV
     return (
-        <div className={'text-center'}>
+        <div className={`text-center ${className}`}>
             <div className={'text'}>Verzió {import.meta.env.VITE_APP_VERSION}</div>
-            {import.meta.env.VITE_APP_ENV !== 'prod'
+            {showEnv && import.meta.env.VITE_APP_ENV !== 'prod'
                 && (<div className={'text text-xs'}>Fejlesztési környezet</div>)}
         </div>
     )

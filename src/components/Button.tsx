efa3b92bcf03c7e1963c1f7eaee7b7b065acecd2
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-21 10:41:12 +0100

    added link to button through prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import classnames from "classnames";
+import { Link } from 'react-router-dom';
+import LinkButton from './LinkButton';
 
 type customColor = {
     fill: string,
@@ -12,9 +14,10 @@ type Props = {
     color?: customColor | undefined,
     text: string,
     className?: string,
+    linkTo?: string | undefined
 }
 
-function Button({ size, style, color = undefined, text, className }: Props) {
+function Button({ size, style, color = undefined, text, className, linkTo = undefined }: Props) {
     const buttonSize = {
         'primary': 'w-80 py-4',
         'secondary': 'w-50 px-14 h-10 leading-10',
@@ -26,12 +29,13 @@ function Button({ size, style, color = undefined, text, className }: Props) {
     const fillColor = color !== undefined ? color.fill : 'bg-pr-blue';
     const hoverColor = color !== undefined ? color.hover : 'hover:bg-pr-blue-light';
     const filledStyle = classnames(fillColor, hoverColor, 'text-white');
-
-    return (
+    const buttonJSXElement = (
         <button className={`${baseStyle} ${className} ${style === 'filled' ? filledStyle : outlinedStyle} ${buttonSize[size]}`}>
             <span>{text.toUpperCase()}</span>
         </button>
     )
+
+    return linkTo !== undefined ? <LinkButton linkTo={linkTo} noStyle={true}>{buttonJSXElement}</LinkButton> : buttonJSXElement;
 }
 
 export default Button

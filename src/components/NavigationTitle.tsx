693b4cfb6e96a9db1cf8e9c416663d333fd146d8
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 01:14:58 +0100

    nav title to only re render this component on navbar

---
+++ src/components/NavigationTitle.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import { config } from '../config/headerConfig';
+import { getURLSegment } from '../utils/url';
+
+const NavigationTitle = () => {
+    const location = useLocation();
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        const segment: string = getURLSegment(location.pathname, null) || '';
+
+        setTitle(config.routeNames[segment]);
+    }, [location]);
+
+    return <>{title}</>;
+}
+
+export default NavigationTitle

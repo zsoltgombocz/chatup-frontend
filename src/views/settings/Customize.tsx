f0dbdc918e7b511e5ff06c93c5cf929ac0e7ccfe
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 14:38:10 +0100

    added theme changer

--- src/views/settings/Customize.tsx
+++ src/views/settings/Customize.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { motion as m } from 'framer-motion';
+import Footer from '../../layout/Footer';
+import Switch from '../../components/Switch';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
+import { setTheme } from '../../utils/theme';
+import { useUserSettings } from '../../store/userSettings';
 
-type Props = {}
-
-const Customize = (props: Props) => {
+const Customize = () => {
+    const switchOptions = [
+        {
+            icon: <SunIcon className={'w-7 h-7'} />,
+            text: 'Világos'
+        }, {
+            icon: <MoonIcon className={'w-7 h-7'} />,
+            text: 'Sötét'
+        }
+    ];
+    const updateTheme = useUserSettings(state => state.setTheme);
+    const handleThemeChange = (index: number) => {
+        updateTheme(index);
+        setTheme(index);
+    }
     return (
-        <div>Customize</div>
-    )
+        <>
+            <m.div className={'scrollable-view'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
+                <h5 className={'text text-xl mb-3'}>Téma választása</h5>
+                <Switch options={switchOptions} onChange={handleThemeChange} className={'mb-3 self-center'} />
+            </m.div>
+        </>);
 }
 
 export default Customize

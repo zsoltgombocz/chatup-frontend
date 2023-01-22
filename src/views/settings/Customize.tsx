7453c35a9f1e06ffaae9c52047764127ad9e8c53
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-23 00:12:40 +0100

    added main color switching

--- src/views/settings/Customize.tsx
+++ src/views/settings/Customize.tsx
@@ -4,9 +4,11 @@ import Switch from '../../components/settings/Switch';
 import { config } from '../../config/settingsConfig';
 import { setTheme } from '../../utils/theme';
 import { useUserSettings } from '../../store/userSettings';
+import ColorSwitcher from '../../components/settings/ColorSwitcher';
 
 const Customize = () => {
     const switchOptions = config.themes;
+    const colorOptions = config.colors;
     const updateTheme = useUserSettings(state => state.setTheme);
     const theme = useUserSettings(state => state.theme);
 
@@ -19,7 +21,8 @@ const Customize = () => {
             <m.div className={'scrollable-view'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
                 <h5 className={'text text-xl mb-3'}>Téma választása</h5>
                 <Switch options={switchOptions} onChange={handleThemeChange} className={'mb-3 self-center'} initialSelectedIndex={theme} />
-                <h5 className={'text text-xl mb-3'}>Téma színének választása</h5>
+                <h5 className={'text text-xl mt-5 mb-3'}>Téma színének választása</h5>
+                <ColorSwitcher colors={colorOptions} />
             </m.div>
         </>);
 }

b65cf9765b6cf43412a6a721cfaf86b7a053d046
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 01:15:09 +0100

    simplified navigation header

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,27 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-import { config } from '../config/headerConfig';
-import { getURLSegment } from '../utils/url';
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/24/outline'
+import NavigationTitle from './NavigationTitle';
 
 function NavigationHeader() {
-    const location = useLocation();
     const navigate = useNavigate();
-
-    const [title, setTitle] = useState('');
-    useEffect(() => {
-        const segment: string = getURLSegment(location.pathname, null) || '';
-
-        setTitle(config.routeNames[segment]);
-    }, [location.pathname]);
 
     const handleBackClick = () => {
         navigate(-1);
     }
     return (
-        <div className={'flex flex-row items-center text'}>
+        <div className={'flex flex-row items-center text text-2xl'}>
             <ChevronLeftIcon className="absolute left-10 h-7 w-7 cursor-pointer" onClick={handleBackClick} />
-            {title}
+            <NavigationTitle />
         </div>
     )
 }

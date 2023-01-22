be10d96d17d430f39acf24f09da05ff0bbaf032f
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 14:41:22 +0100

    toggle switcher, currently tested with 2 options

---
+++ src/components/Switch.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode, useEffect, useState } from 'react'
+import { motion as m } from 'framer-motion';
+
+type option = {
+    icon: ReactNode,
+    text: string
+}
+type Props = {
+    options: option[] | [],
+    onChange?: Function | undefined,
+    className?: string,
+}
+
+function Switch({ options, onChange = undefined, className }: Props) {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    useEffect(() => {
+    }, [options, onChange]);
+
+    const handleOnClick = (index: number) => {
+        if (index > options.length || index < 0) { return; }
+        setSelectedIndex(index);
+
+        onChange?.(index);
+    }
+
+
+    return (
+        <div className={'border border-1  border-gray-600 flex flex-row rounded-full w-fit p-1 relative ' + className}>
+            <m.div
+                animate={{ left: `calc(6rem * ${selectedIndex})` }}
+                className={'z-0 w-24 border-1 border border-gray-600 absolute h-[calc(100%_-_0.25rem)] rounded-full -top-[0.125rem] bg-white m-1'}></m.div>
+            {
+                options.map((opt, i) =>
+                    <div className={'z-10 px-2 py-1 flex text-gray-600 rounded-full justify-center items-center gap-1 w-24 cursor-pointer'} onClick={() => handleOnClick(i)}>
+                        <span>{opt.icon}</span>
+                        {selectedIndex === i ? <span className={"font-medium mr-1"}>{opt.text}</span> : <></>}
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Switch

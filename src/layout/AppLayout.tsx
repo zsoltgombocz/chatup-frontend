622de1fe3082a67e1651da0be0da656ae59ba960
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 14:40:24 +0100

    render static footer when needed

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,18 +1,32 @@
 import { AnimatePresence } from 'framer-motion';
-import React from 'react'
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from './Footer';
 import Header from './Header';
+import { config } from '../config/footerConfig';
+import { getURLSegment } from '../utils/url';
 
 type Props = {
     disableLayout?: boolean
 }
 const AppLayout = ({ disableLayout = false }: Props) => {
+    const location = useLocation();
+    const [currentMainRoute, setCurrentMainRoute] = useState('');
+    useEffect(() => {
+        const segment = getURLSegment(location.pathname, 0);
+
+        if (segment !== undefined)
+            setCurrentMainRoute(segment);
+    }, [location.pathname])
+
+
     return disableLayout ? <Outlet /> : (
         <div className={'bg-bg-light-inner dark:bg-bg-dark-inner flex flex-col w-full h-full'}>
             <Header />
             <AnimatePresence>
                 <Outlet />
             </AnimatePresence>
+            {config.useStaticFooterRoutes.includes(currentMainRoute) && <Footer />}
         </div>);
 }
 

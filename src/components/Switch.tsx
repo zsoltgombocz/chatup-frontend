1bdfa083e3a748477414b084e51730e5ce88c1f3
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 14:45:53 +0100

    added initially selected index prop

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -9,21 +9,22 @@ type Props = {
     options: option[] | [],
     onChange?: Function | undefined,
     className?: string,
+    initialSelectedIndex?: number,
 }
 
-function Switch({ options, onChange = undefined, className }: Props) {
+function Switch({ options, onChange = undefined, className, initialSelectedIndex = 0 }: Props) {
     const [selectedIndex, setSelectedIndex] = useState(0);
 
     useEffect(() => {
+        selectOption(initialSelectedIndex);
     }, [options, onChange]);
 
-    const handleOnClick = (index: number) => {
+    const selectOption = (index: number) => {
         if (index > options.length || index < 0) { return; }
         setSelectedIndex(index);
 
         onChange?.(index);
     }
-
 
     return (
         <div className={'border border-1  border-gray-600 flex flex-row rounded-full w-fit p-1 relative ' + className}>
@@ -32,7 +33,7 @@ function Switch({ options, onChange = undefined, className }: Props) {
                 className={'z-0 w-24 border-1 border border-gray-600 absolute h-[calc(100%_-_0.25rem)] rounded-full -top-[0.125rem] bg-white m-1'}></m.div>
             {
                 options.map((opt, i) =>
-                    <div className={'z-10 px-2 py-1 flex text-gray-600 rounded-full justify-center items-center gap-1 w-24 cursor-pointer'} onClick={() => handleOnClick(i)}>
+                    <div key={i + '' + opt.text} className={'z-10 px-2 py-1 flex text-gray-600 rounded-full justify-center items-center gap-1 w-24 cursor-pointer'} onClick={() => selectOption(i)}>
                         <span>{opt.icon}</span>
                         {selectedIndex === i ? <span className={"font-medium mr-1"}>{opt.text}</span> : <></>}
                     </div>

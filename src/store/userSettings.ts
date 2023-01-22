ed2af443e548cf04e57a5017b3df47f2900b77e1
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-23 00:12:52 +0100

    added color and theme saving + load

--- src/store/userSettings.ts
+++ src/store/userSettings.ts
@@ -3,21 +3,33 @@ import { create } from 'zustand';
 interface UserSettingsInterface {
     theme: number,
     color: string,
-    setTheme: (index: number) => void
+    setTheme: (index: number) => void,
+    setColor: (color: string) => void
 }
 
 const getTheme = (): number => {
     const theme: string | null = localStorage.getItem('chatup_theme');
-    console.log('no saved theme found');
+
     if (theme === null) return 0;
     else return parseInt(theme);
 }
 
+const getColor = (): string => {
+    const color: string | null = localStorage.getItem('chatup_color');
+
+    if (color === null) return 'blue';
+    else return color;
+}
+
 export const useUserSettings = create<UserSettingsInterface>((set) => ({
     theme: getTheme(),
-    color: "",
+    color: getColor(),
     setTheme: (index: number) => {
         localStorage.setItem('chatup_theme', '' + index);
         set(state => ({ ...state, theme: index }))
+    },
+    setColor: (color: string) => {
+        localStorage.setItem('chatup_color', color);
+        set(state => ({ ...state, color }))
     }
 }));

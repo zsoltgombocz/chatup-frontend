4d265867aff06c3359cd04ce137087e8feec2867
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-22 01:01:44 +0100

    added sub routes to settings

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeView from './views/HomeView';
 import { Routes, Route, useLocation } from "react-router-dom";
 import AppLayout from './layout/AppLayout';
-import SettingsView from './views/SettingsView';
+import SettingsView from './views/settings/SettingsView';
+import { AnimatePresence } from 'framer-motion';
+import Privacy from './views/settings/Privacy';
+import Customize from './views/settings/Customize';
+import Information from './views/settings/Information';
+import Help from './views/settings/Help';
+import Contact from './views/settings/Contact';
 
 const Application = () => {
 
     const location = useLocation();
+
+    useEffect(() => {
+        console.log('app mount');
+
+        return () => {
+            console.log('app unmount')
+        }
+    }, [])
 
     return (
         <Routes>
@@ -14,7 +28,11 @@ const Application = () => {
                 <Route index element={<HomeView />} />
                 <Route path="settings">
                     <Route index element={<SettingsView />} />
-                    <Route path={'privacy'} element={<SettingsView />} />
+                    <Route path={'privacy'} element={<Privacy />} />
+                    <Route path={'customize'} element={<Customize />} />
+                    <Route path={'information'} element={<Information />} />
+                    <Route path={'help'} element={<Help />} />
+                    <Route path={'contact'} element={<Contact />} />
                 </Route>
             </Route>
         </Routes>

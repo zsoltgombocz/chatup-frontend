1e3801b50d0b425db78939b51738680d83dc7afd
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-24 00:47:30 +0100

    added gap between setting categories

--- src/views/settings/SettingsView.tsx
+++ src/views/settings/SettingsView.tsx
@@ -7,7 +7,7 @@ import { motion as m } from 'framer-motion';
 function SettingsView() {
     return (
         <>
-            <m.div className={'scrollable-view'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
+            <m.div className={'scrollable-view gap-5'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
                 {config.categories.map(category =>
                     <Category key={category.name} icon={category.icon} text={category.display} redirect={category.route} />
                 )}

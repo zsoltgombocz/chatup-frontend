81d780d588129208cf18cf05acc6336b9f27aa97
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-24 11:50:10 +0100

    removed empty tag

--- src/views/settings/Customize.tsx
+++ src/views/settings/Customize.tsx
@@ -17,14 +17,13 @@ const Customize = () => {
         setTheme(index);
     }
     return (
-        <>
-            <m.div className={'scrollable-view'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
-                <h5 className={'text text-xl mb-3'}>Téma választása</h5>
-                <Switch options={switchOptions} onChange={handleThemeChange} className={'mb-3 self-center'} initialSelectedIndex={theme} />
-                <h5 className={'text text-xl mt-5 mb-3'}>Téma színének választása</h5>
-                <ColorSwitcher colors={colorOptions} />
-            </m.div>
-        </>);
+        <m.div className={'scrollable-view'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
+            <h5 className={'text text-xl mb-3'}>Téma választása</h5>
+            <Switch options={switchOptions} onChange={handleThemeChange} className={'mb-3 self-center'} initialSelectedIndex={theme} />
+            <h5 className={'text text-xl mt-5 mb-3'}>Téma színének választása</h5>
+            <ColorSwitcher colors={colorOptions} />
+        </m.div>
+    );
 }
 
 export default Customize
--- src/views/settings/SettingsView.tsx
+++ src/views/settings/SettingsView.tsx
@@ -6,13 +6,12 @@ import { motion as m } from 'framer-motion';
 
 function SettingsView() {
     return (
-        <>
-            <m.div className={'scrollable-view gap-5'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
-                {config.categories.map(category =>
-                    <Category key={category.name} icon={category.icon} text={category.display} redirect={category.route} />
-                )}
-            </m.div>
-        </>);
+        <m.div className={'scrollable-view gap-5'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
+            {config.categories.map(category =>
+                <Category key={category.name} icon={category.icon} text={category.display} redirect={category.route} />
+            )}
+        </m.div>
+    );
 }
 
 export default SettingsView;

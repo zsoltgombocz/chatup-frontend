0466075156450f8c1bbad5c3726276a6e467c2d3
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-24 11:51:20 +0100

    rendered links

--- src/views/settings/Information.tsx
+++ src/views/settings/Information.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { motion as m } from 'framer-motion';
+import { config } from '../../config/settingsConfig';
+import Category from '../../components/settings/Category';
 
-type Props = {}
-
-function Information({ }: Props) {
+function Information() {
     return (
-        <div>Information</div>
+        <m.div className={'scrollable-view gap-5'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
+            {config.informationLinks.map(link => <Category text={link.display} redirect={link.route} />)}
+        </m.div>
     )
 }
 

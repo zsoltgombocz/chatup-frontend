fd76b014076843c58f183895c7287db82a3718f4
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-24 22:29:19 +0100

    added help subpage listing all faq

--- src/views/settings/Help.tsx
+++ src/views/settings/Help.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { motion as m } from 'framer-motion';
+import faq from '../../config/FAQ.json'
+import LinkButton from '../../components/LinkButton';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
-type Props = {}
-
-const Help = (props: Props) => {
+const Help = () => {
     return (
-        <div>Help</div>
+        <m.div className={'scrollable-view'} initial={{ x: 500 }} animate={{ x: 0 }} exit={{ x: 500 }}>
+            <h5 className={'text text-xl mb-3 text-center'}>Gyakran ismételt kérdések</h5>
+            {
+                faq.map(qa => (
+                    <div className={'mb-6'}>
+                        <p className={'text mb-1'}>{qa.q}</p>
+                        <p className={'text text-justify pl-2 font-extralight'}>{qa.a}</p>
+                    </div>
+                ))
+            }
+            <LinkButton linkTo={'/settings/contact'}>
+                <div className={'flex flex-row gap-1 items-center'}>Segítség kérése <ChevronRightIcon className={'w-4 h-4'} /></div>
+            </LinkButton>
+        </m.div>
     )
+
 }
 
 export default Help

001953ace6acfa973922931e3dcd3a4d6dcc3e70
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-25 23:11:36 +0100

    added ability to disable route name

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/24/outline'
 import NavigationTitle from './NavigationTitle';
 
-function NavigationHeader() {
+type Props = {
+    hideRouteText: boolean
+}
+
+const NavigationHeader = ({ hideRouteText = false }: Props) => {
     const navigate = useNavigate();
 
     const handleBackClick = () => {
@@ -12,7 +16,7 @@ function NavigationHeader() {
     return (
         <div className={'flex flex-row items-center text text-2xl'}>
             <ChevronLeftIcon className="absolute left-10 h-7 w-7 cursor-pointer" onClick={handleBackClick} />
-            <NavigationTitle />
+            {!hideRouteText ? <NavigationTitle /> : <div className={'opacity-0'}>ROUTE</div>}
         </div>
     )
 }

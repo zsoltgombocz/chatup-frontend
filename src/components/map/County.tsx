75b61f142a2fa4e34341814f669d3be44b34da92
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-26 23:58:13 +0100

    added onclick function + selected prop

--- src/components/map/County.tsx
+++ src/components/map/County.tsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useUserSettings } from '../../store/userSettings';
-import { AnimatePresence, motion as m } from 'framer-motion';
+import { motion as m } from 'framer-motion';
 
 type Props = {
     path: string,
     id: string,
+    onCountyClicked: Function | undefined,
+    selected: boolean
 }
 
-function County({ path, id }: Props) {
-    const [selected, setSelected] = useState(false);
+function County({ path, id, onCountyClicked, selected }: Props) {
+    const [countyState, setCountyState] = useState(selected);
     const userColor = useUserSettings(state => state.color);
-    useEffect(() => {
-        console.log(selected);
-    }, [selected])
 
     return (
         <m.path
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0 }}
-            onClick={() => setSelected(!selected)}
-            className={`cursor-pointer county stroke-bg-dark-outer dark:stroke-bg-light-outer stroke-1 ${selected ? 'fill-' + userColor : 'fill-bg-light-inner dark:fill-bg-dark-inner'}`}
+            onClick={() => {
+                const state = !countyState;
+                onCountyClicked?.(id, state);
+                setCountyState(state);
+            }}
+            className={`cursor-pointer county stroke-bg-dark-outer dark:stroke-bg-light-outer stroke-1 ${countyState ? 'fill-' + userColor : 'fill-bg-light-inner dark:fill-bg-dark-inner'}`}
             d={path} id={id}>
         </m.path >
     )

4c53f1344e2f7e69c1293c5104df16aa9307698a
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-26 19:20:22 +0100

    pre chat view, gives back view based on query param

--- src/views/PreChatView.tsx
+++ src/views/PreChatView.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { motion as m } from 'framer-motion';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import Map from '../components/map/Map';
+import { useSearchParams } from 'react-router-dom';
+import CountySelectionView from './CountySelectionView';
+import SexSelectionView from './SexSelectionView';
+import InterestSelectionView from './InterestSelectionView';
 function PreChatView() {
-    return (
-        <m.div className={'scrollable-view'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
-            Pre
-        </m.div>
-    );
+    const [searcParams] = useSearchParams();
+    const [t, setT] = useState<null | string>(null);
+
+    useEffect(() => {
+        setT(searcParams.get('t'));
+    }, [searcParams.get('t')]);
+
+    switch (t) {
+        case 'county':
+            return <CountySelectionView />;
+        case 'sex':
+            return <SexSelectionView />;
+        case 'interest':
+            return <InterestSelectionView />;
+        default:
+            return <CountySelectionView />;
+    }
 }
 export default PreChatView

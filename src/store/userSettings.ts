2b6c4e3d4922c30370964d840d96e125f15213d7
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-27 00:00:07 +0100

    map state global state

--- src/store/userSettings.ts
+++ src/store/userSettings.ts
@@ -1,12 +1,24 @@
 import { create } from 'zustand';
+import { County, CountyInterface } from '../utils/map.types';
 
+interface CountyStateInterface {
+    id: County,
+    selected: boolean,
+}
+
+interface MapStateInterface {
+    counties: CountyStateInterface[],
+    allSelected: boolean,
+}
 interface UserSettingsInterface {
     theme: number,
     color: string,
     privacy: boolean[],
+    map: MapStateInterface,
     setTheme: (index: number) => void,
     setColor: (color: string) => void,
     setPrivacy: (index: number, b: boolean) => void,
+    setMap: (countyId: County | 'all', b: boolean) => void
 }
 
 const getTheme = (): number => {
@@ -34,6 +46,10 @@ export const useUserSettings = create<UserSettingsInterface>((set, get) => ({
     theme: getTheme(),
     color: getColor(),
     privacy: getPrivacy(),
+    map: {
+        counties: [],
+        allSelected: false
+    },
     setTheme: (index: number) => {
         localStorage.setItem('chatup_theme', '' + index);
         set(state => ({ ...state, theme: index }))
@@ -47,5 +63,18 @@ export const useUserSettings = create<UserSettingsInterface>((set, get) => ({
         current[index] = b;
         localStorage.setItem('chatup_privacy', JSON.stringify(current));
         set(state => ({ ...state, privacy: current }));
-    }
+    },
+    setMap: (countyId: County | 'all', b: boolean) => {
+        if (countyId === 'all') set(state => ({ ...state, map: { counties: [], allSelected: true } }));
+        const countyExist: number = get().map.counties.findIndex(c => c.id === countyId);
+
+        let counties: CountyStateInterface[] = get().map.counties;
+        if (countyExist === -1 && countyId !== 'all') {
+            counties.push({ id: countyId, selected: b });
+        } else {
+            counties[countyExist].selected = b;
+        }
+        set(state => ({ ...state, map: { allSelected: false, counties } }))
+        console.log(get().map);
+    },
 }));

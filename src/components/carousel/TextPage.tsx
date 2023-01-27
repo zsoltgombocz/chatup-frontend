42aa1245249526f6b578beac93f81455788ed3af
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-27 19:58:51 +0100

    smaller text size

--- src/components/carousel/TextPage.tsx
+++ src/components/carousel/TextPage.tsx
@@ -8,6 +8,6 @@ type Props = {
 export const TextPage = ({ title, text }: Props) => {
     return (<div>
         <h5 className={'text-cabin text-base font-bold text-center mb-3'}>{title}</h5>
-        <p className={'text-cabin font-extralight text-base text-center leading-relaxed'}>{text}</p>
+        <p className={'text-cabin font-extralight text-sm text-center leading-relaxed'}>{text}</p>
     </div>)
 }

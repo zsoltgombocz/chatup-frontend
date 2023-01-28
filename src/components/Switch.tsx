d3c8cea672459f949408dbeab5b6020290528b94
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-28 22:12:14 +0100

    used global switch option interface + text not neccessary

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,12 +1,10 @@
 import React, { ReactNode, useEffect, useState } from 'react'
 import { motion as m } from 'framer-motion';
+import { SwitchOptionInterface } from '../utils/interfaces/components/switch';
 
-type option = {
-    icon: ReactNode,
-    text: string
-}
+
 type Props = {
-    options: option[] | [],
+    options: SwitchOptionInterface[] | [],
     onChange?: Function | undefined,
     className?: string,
     initialSelectedIndex?: number,
@@ -33,9 +31,9 @@ function Switch({ options, onChange = undefined, className, initialSelectedIndex
                 className={'z-0 w-24 border-1 border border-gray-600 absolute h-[calc(100%_-_0.25rem)] rounded-full -top-[0.125rem] bg-white m-1'}></m.div>
             {
                 options.map((opt, i) =>
-                    <div key={i + '' + opt.text} className={'z-10 px-2 py-1 flex text-gray-600 rounded-full justify-center items-center gap-1 w-24 cursor-pointer'} onClick={() => selectOption(i)}>
+                    <div key={i + '' + (opt.text || 'text')} className={'z-10 px-2 py-1 flex text-gray-600 rounded-full justify-center items-center gap-1 w-24 cursor-pointer'} onClick={() => selectOption(i)}>
                         <span>{opt.icon}</span>
-                        {selectedIndex === i ? <span className={"font-medium mr-1"}>{opt.text}</span> : <></>}
+                        {selectedIndex === i && opt.text ? <span className={"font-medium mr-1"}>{opt.text}</span> : <></>}
                     </div>
                 )
             }

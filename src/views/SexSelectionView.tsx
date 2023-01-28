fcf391966eb593ad17808c0eee521d5391cb02ae
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-28 22:24:01 +0100

    updated layout

--- src/views/SexSelectionView.tsx
+++ src/views/SexSelectionView.tsx
@@ -23,15 +23,23 @@ function SexSelectionView() {
     }
     return (
         <>
-            <m.div className={'scrollable-view !px-0 !pt-1'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
-                <h5 className={'text-cabin font-semibold text-center text-xl mb-3'}>Válaszd ki a nemed:</h5>
-                <p className={'font-light text-sm text text-center mb-6'}>Kérjük, hogy a valós nemed add meg a komoly és eredménydús beszélgetés érdekében.</p>
+            <m.div className={'scrollable-view'} initial={{ x: -500 }} animate={{ x: 0 }} exit={{ x: -500 }}>
+                <div className={'flex flex-col content-between flex-grow'}>
+                    <div className={'flex flex-col flex-grow content-evenly mb-8'}>
+                        <div className={'flex flex-col flex-auto'}>
+                            <h5 className={'text-cabin font-semibold text-center text-xl mb-3'}>Válaszd ki a nemed:</h5>
+                            <p className={'font-light text-sm text text-center mb-6'}>Kérjük, hogy a valós nemed add meg a komoly és eredménydús beszélgetés érdekében.</p>
 
-                <Switch options={config.ownSex} className={'mb-12 self-center'} initialSelectedIndex={ownSexState} onChange={ownSexChanged} />
-                <h5 className={'text-cabin font-semibold text-center text-xl mb-6'}>Kivel szeretnél beszélgetni:</h5>
+                            <Switch options={config.ownSex} className={'mb-6 self-center'} initialSelectedIndex={ownSexState} onChange={ownSexChanged} />
+                        </div>
+                        <div className={'flex flex-col flex-auto'}>
+                            <h5 className={'text-cabin font-semibold text-center text-xl mb-6'}>Kivel szeretnél beszélgetni:</h5>
+                            <Switch options={config.partnerSex} className={'mb-6 self-center'} initialSelectedIndex={partnerSexState} onChange={partnerSexChanged} />
+                        </div>
+                    </div>
 
-                <Switch options={config.partnerSex} className={'mb-16 self-center'} initialSelectedIndex={partnerSexState} onChange={partnerSexChanged} />
-                <TextCarousel data={sexCarouselData} className={'!px-10 mt-10'} />
+                    <TextCarousel data={sexCarouselData} className={'mt-10'} />
+                </div>
             </m.div>
             <Footer showVersion={false}>
                 <Button size={'primary'} style={'filled'} text={'tovább'} linkTo={'/pre?t=interest'} className={'mb-3'} />

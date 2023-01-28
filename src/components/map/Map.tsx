c35591fed3255f02218f1472c7617403c08b1f49
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-28 18:19:44 +0100

    added new store functions + rendering based on that store

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -3,30 +3,31 @@ import { MapStructure } from '../../config/mapConfig';
 import County from './County';
 import { motion as m } from 'framer-motion';
 import { County as CountyEnum, CountyInterface } from '../../utils/map.types';
-import { useUserSettings } from '../../store/userSettings';
+import { useMapPreferences } from '../../store/mapPreferences';
 
 type Props = {
   className?: string,
 }
 
 const Map = ({ className }: Props) => {
-  const setCounty = useUserSettings(state => state.setMap);
-  const mapState = useUserSettings(state => state.map);
+  const updateCounty = useMapPreferences(state => state.updateCounty);
+  const counties = useMapPreferences(state => state.counties);
+  const checkbox = useMapPreferences(state => state.mapCheckbox);
 
   const onCountyClicked = (id: CountyEnum, b: boolean) => {
-    if (mapState.allSelected) {
-      setCounty('all', false);
+    if (checkbox === 0) {
+      updateCounty('all', false);
     }
 
-    setCounty(id, b);
+    updateCounty(id, b);
   }
   return (
     <m.svg className={'map ' + className} xmlns-svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" id="svg2" viewBox="-25 -100 1100 900" width={'100%'}>
       {MapStructure.counties.map((county: CountyInterface) =>
         <County
           key={county.id}
-          disabled={mapState.allSelected}
-          selected={mapState.allSelected ? true : (mapState.counties.filter(c => c.id === county.id)[0]?.selected || false)}
+          disabled={checkbox === 0}
+          selected={checkbox === 0 ? true : (counties.includes(county.id) || false)}
           onCountyClicked={onCountyClicked}
           path={county.path} id={county.id}
         />)}

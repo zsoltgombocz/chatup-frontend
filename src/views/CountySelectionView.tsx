f4b6d3f4d581e6df932f694bd44d9d79b6877d5e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-02 18:47:06 +0100

    moved TextCarousel -> changed import path

--- src/views/CountySelectionView.tsx
+++ src/views/CountySelectionView.tsx
@@ -1,7 +1,7 @@
 import React, { SyntheticEvent } from 'react'
 import { motion as m } from 'framer-motion';
 import Footer from '../layout/Footer';
-import TextCarousel from '../components/carousel/TextCarousel';
+import TextCarousel from '../components/textCarousel/TextCarousel';
 import countyCarouselData from '../config/carousels/county.json';
 import Map from '../components/map/Map';
 import RadioGroup from '../components/RadioGroup';

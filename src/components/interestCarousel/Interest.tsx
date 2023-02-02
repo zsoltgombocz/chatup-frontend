5db2cf1c1ff750845bfd362fc51550fa756b358b
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-02 22:09:00 +0100

    added single standalone interest component

---
+++ src/components/interestCarousel/Interest.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { motion as m } from 'framer-motion';
+import { useUserSettings } from '../../store/userSettings';
+
+type Props = {
+    src: string | undefined,
+    display: string,
+    defaultSelected?: boolean
+
+}
+
+const Interest = ({ src, display, defaultSelected = false }: Props) => {
+    const [selected, setSelected] = useState(defaultSelected);
+    const userColor = useUserSettings(state => state.color);
+    return (
+        <div className={'flex flex-col p-1 w-32 h-40 items-evenly justify-evenly select-none'} >
+            <div className={'relative flex items-center justify-center'}>
+                <img
+                    src={src}
+                    className={`cursor-pointer interest border-color-blue z-50 ${selected ? 'saturate-100' : 'saturate-0'}`}
+                    onClick={() => setSelected(!selected)}
+                />
+                <m.div animate={selected ? { opacity: 1 } : { opacity: 0 }} className={`bg-${userColor} absolute z-0 rounded-full w-[114px] h-[114px]`} />
+            </div>
+            <span className={'text text-base text-center'}>{display}</span>
+        </div>
+    )
+}
+
+export default Interest

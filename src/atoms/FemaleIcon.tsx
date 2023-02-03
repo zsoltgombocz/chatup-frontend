04f7d9975f2339c6148f6d10c45542b37513c07d
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-03 23:56:19 +0100

    fixed warning

--- src/atoms/FemaleIcon.tsx
+++ src/atoms/FemaleIcon.tsx
@@ -6,7 +6,7 @@ const FemaleIcon = ({ size = 16 }: Props) => {
     return (
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="currentColor" className="bi bi-gender-female" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z" />
+                <path fillRule="evenodd" d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z" />
             </svg>
         </div>
     )

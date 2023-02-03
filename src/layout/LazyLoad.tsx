16f65d15c75a446ca74e5520ad418320e4fd805b
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-03 21:46:32 +0100

    added lazy loading loader

--- src/layout/LazyLoad.tsx
+++ src/layout/LazyLoad.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode, Suspense } from 'react'
+import LoadingIcon from '../atoms/LoadingIcon'
 
 type Props = {
   children: string | ReactNode | undefined
 }
 
 const LazyLoad = ({ children }: Props) => {
-  return <Suspense fallback={<>loading</>}>{children}</Suspense>
+  return <Suspense fallback={
+    <div className={'h-full w-full flex items-center justify-center'}><LoadingIcon /></div>}>
+    {children}
+  </Suspense>;
 }
 
 export default LazyLoad

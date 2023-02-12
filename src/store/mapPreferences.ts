d8401e4c9bd98c79c65a18404c938db3eae0ec7f
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-12 16:25:16 +0100

    simplified store + added cookie save

--- src/store/mapPreferences.ts
+++ src/store/mapPreferences.ts
@@ -1,30 +1,42 @@
 import { create } from 'zustand';
 import { County } from '../utils/interfaces/map';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 interface MapPreferencesInterface {
-    mapCheckbox: 0 | 1,
+    mapCheckbox: 0 | 1, // 0 - countrywide, 1 - only selected counties
     counties: County[],
     setTickedCheckbox: (cb: 0 | 1) => void,
-    updateCounty: (id: County | 'all', b: boolean) => void,
+    updateCounty: (id: County, b: boolean) => void,
+}
+
+const getStoredCheckboxValue = (): (0 | 1) => {
+    const storedValue: null | string = cookies.get('chatup_map_checkbox');
+
+    if (storedValue === null) return 0;
+
+    const value = parseInt(storedValue);
+
+    return value === 1 ? 1 : 0;
 }
 
 export const useMapPreferences = create<MapPreferencesInterface>((set, get) => ({
-    counties: [],
-    mapCheckbox: 0,
+    counties: cookies.get('chatup_map_counties') || [],
+    mapCheckbox: getStoredCheckboxValue(),
+
     setTickedCheckbox: (cb: 0 | 1) => {
+        cookies.set('chatup_map_checkbox', cb);
         set(state => ({ ...state, mapCheckbox: cb }));
     },
-    updateCounty: (id: County | 'all', b: boolean) => {
-        if (id === 'all') {
-            set(state => ({ ...state, map: { counties: [], allSelected: b } }));
+    updateCounty: (id: County, b: boolean) => {
+        if (b) {
+            set(state => ({ ...state, counties: [...state.counties, id] }));
         } else {
-            if (b) {
-                set(state => ({ ...state, counties: [...state.counties, id] }));
-            } else {
-                const filteredCounties = get().counties.filter(county => county !== id);
-                set(state => ({ ...state, counties: filteredCounties }));
-            }
-            console.log(get().counties);
+            const filteredCounties = get().counties.filter(county => county !== id);
+            set(state => ({ ...state, counties: filteredCounties }));
         }
+
+        cookies.set('chatup_map_counties', get().counties);
     }
 }));

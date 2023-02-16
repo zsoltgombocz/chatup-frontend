b13ca93a6a7bbc97ee161652f7188ea9747822a4
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-16 19:58:56 +0100

    Cleaning + fallback api request
    Sometimes reverse geo locating does not gives back county, so calling the api with text search to get more information, if still not getting county returning with null and the app considering the location as everywhere (just like when declining permission to get location)

--- src/hooks/useGeoLocation.ts
+++ src/hooks/useGeoLocation.ts
@@ -2,30 +2,51 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { counties } from "../config/mapConfig";
 import { useUserData } from "../store/userData";
 import { GeoLocationPermission } from "../utils/enums";
-import { GeoLocation } from "../utils/interfaces/geoLocation";
 import { CountyInterface } from "../utils/interfaces/map";
 import { isSimilar } from "../utils/string";
+interface GeoLocationInterface {
+    lat: number,
+    lon: number,
 
+}
 interface useGeoLocationInterface {
     permission: GeoLocationPermission,
-    location: null | CountyInterface,
+    location: null | CountyInterface | undefined,
 }
+
+const API_URL = import.meta.env.VITE_GEOAPI_URL;
+const API_KEY = import.meta.env.VITE_GEOAPI_KEY;
 
 export const useGeoLocation = (): useGeoLocationInterface => {
     const [permission, setPermission] = useState<GeoLocationPermission>(GeoLocationPermission.DENIED);
     const setUserLocation = useUserData(state => state.setUserLocation);
     const userLocation = useUserData(state => state.location);
 
-    const geoLocation = useCallback(async (location: GeoLocation) => {
-        fetch(`https://api.geoapify.com/v1/geocode/reverse?apiKey=${import.meta.env.VITE_GEOAPI_KEY}&lat=${location?.lat}&lon=${location?.lon}`)
+    const geoLocation = useCallback(async (location: GeoLocationInterface) => {
+        fetch(`${API_URL}/reverse?apiKey=${API_KEY}&lat=${location?.lat}&lon=${location?.lon}`)
             .then(response => response.json())
-            .then(result => {
-                const county = result.features[0].properties.county;
-                const similar = counties.filter(c => isSimilar(c.name, county));
-                setUserLocation(similar[0]);
+            .then(async result => {
+                const county: string | null = result.features[0].properties.county || await getCountyBasedoOnFormattedText(result.features[0].properties.formatted);
+                let userCounty: CountyInterface | null;
+                if (county === null) userCounty = null;
+                else userCounty = counties.filter(c => isSimilar(c.name, county))[0];
+
+                setUserLocation(userCounty);
             })
             .catch(err => console.log(err))
     }, []);
+
+    //Emergency call when no county found in reverse geolocation
+    const getCountyBasedoOnFormattedText = (text: string): Promise<null | string> => {
+        return new Promise(async (resolve, reject) => {
+            fetch(`${API_URL}/search?apiKey=${API_KEY}&text=${text}`)
+                .then(response => response.json())
+                .then(result => {
+                    resolve(result.features[0].properties.county || null);
+                })
+                .catch(err => resolve(null));
+        });
+    }
 
     const getLocation = (): void => {
         navigator.geolocation.getCurrentPosition(
@@ -48,7 +69,7 @@ export const useGeoLocation = (): useGeoLocationInterface => {
 
     useEffect(() => {
         if ((permission === GeoLocationPermission.PROMPT || permission === GeoLocationPermission.GRANTED)
-            && userLocation === null) {
+            && (userLocation === undefined || userLocation === null)) {
             getLocation();
         }
     }, [permission]);

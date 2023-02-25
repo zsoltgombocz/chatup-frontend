7a4e6e551f3987142188bc6c014c134e195edf81
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-25 19:17:01 +0100

    Added more pixel to select circle

--- src/components/carousels/interestCarousel/Interest.tsx
+++ src/components/carousels/interestCarousel/Interest.tsx
@@ -32,7 +32,7 @@ const Interest = ({ id, src, display, defaultSelected = false, disabled = false 
                     className={`${disabled ? 'cursor-not-allowed' : 'cursor-pointer'} interest border-color-blue z-50 ${selected ? 'saturate-100' : 'saturate-0'}`}
                     onClick={onInterestClick}
                 />
-                <m.div initial={{ opacity: 0 }} animate={selected ? { opacity: 1 } : { opacity: 0 }} className={`bg-${userColor} absolute z-0 rounded-full w-[114px] h-[114px]`} />
+                <m.div initial={{ opacity: 0 }} animate={selected ? { opacity: 1 } : { opacity: 0 }} className={`bg-${userColor} absolute z-0 rounded-full w-[116px] h-[116px]`} />
             </div>
             <span className={'text text-base text-center'}>{display}</span>
         </div>

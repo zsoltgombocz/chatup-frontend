700fc1fd3f9603a854eebdd11752436414f2d45a
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-26 17:38:33 +0100

    fix dark mode loader background

--- src/layout/LazyLoad.tsx
+++ src/layout/LazyLoad.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const LazyLoad = ({ children }: Props) => {
   return <Suspense fallback={
-    <div className={'h-full w-full flex items-center justify-center'}><LoadingIcon /></div>}>
+    <div className={'bg-bg-light-inner dark:bg-bg-dark-inner h-full w-full flex items-center justify-center'}><LoadingIcon /></div>}>
     {children}
   </Suspense>;
 }

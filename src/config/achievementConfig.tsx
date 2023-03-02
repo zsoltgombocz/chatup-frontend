3c968cc342609c629fb9c43b9ec0d08a9579e4f0
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-02 13:00:35 +0100

    added another achievement [WIP]

--- src/config/achievementConfig.tsx
+++ src/config/achievementConfig.tsx
@@ -2,6 +2,7 @@ export interface achievementInterface {
     id: string,
     title: string,
     description: string,
+    sound?: string
 }
 
 interface achievementConfigInterface {
@@ -13,7 +14,13 @@ export const config: achievementConfigInterface = {
         {
             id: 'dua_lipa',
             title: 'Dua Lipa itt?',
-            description: 'Gratulálunk! Megtaláltad az első elérhető eredményt. Élvezd ki a különleges témádat mert nem sokan dicsekedhetnek el vele.'
+            description: 'Gratulálunk! Megtaláltad az első elérhető eredményt. Élvezd ki a különleges témádat mert nem sokan dicsekedhetnek el vele.',
+            sound: 'dua_lipa.mp3'
+        },
+        {
+            id: 'enthusiastic_chatter',
+            title: 'Lelkes csevegő!',
+            description: 'Ne hagyd abba! Hamarosan lemerülsz de te mégis a társaságot választod, gratulálunk!',
         }
     ]
 }

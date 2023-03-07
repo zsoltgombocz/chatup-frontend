45203b6a6b881bc048630603827de3c42e81adc0
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-03-07 15:10:52 +0100

    refactored theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,47 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   darkMode: 'class',
-  mode: 'jit',
   content: [
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
+    colors: {
+      ...colors,
+      'primary': {
+        blue: {
+          light: '#77BFC9',
+          DEFAULT: '#34ACBC',
+          dark: '#278591'
+        },
+        red: {
+          light: '#F16158',
+          DEFAULT: '#A91B12',
+          dark: '#87130c'
+        },
+      },
+      gray: {
+        200: '#DBDBDB',
+        300: '#626262',
+        400: '#474747',
+        500: '#3B3B3B',
+        600: '#393939',
+      },
+      'bg-light-inner': '#DBDBDB',
+      'bg-light-outer': '#FFFFFF',
+      'bg-dark-inner': '#282C2D',
+      'bg-dark-outer': '#3B3B3B',
+      'status-red': '#B03737',
+      'status-orange': '#B08E37',
+      'status-green': '#24FF00'
+    },
     extend: {
       fontFamily: {
         'logo': ['Black Ops One', 'sans'],
         'pridi': ['Pridi', 'sans'],
         'cabin': ['Cabin Condensed', 'sans'],
-      },
-      colors: {
-        'pr-red': {
-          light: '#F16158',
-          DEFAULT: '#A91B12',
-          dark: '#87130c'
-        },
-        'pr-blue': {
-          light: '#77BFC9',
-          DEFAULT: '#34ACBC',
-          dark: '#278591'
-        },
-        'pr-green': {
-          light: '#8AF1B9',
-          DEFAULT: '#42AB73',
-          dark: '#2e7850',
-        },
-        gray: {
-          200: '#DBDBDB',
-          300: '#626262',
-          400: '#474747',
-          500: '#3B3B3B',
-          600: '#393939',
-        },
-        'bg-light-inner': '#DBDBDB',
-        'bg-light-outer': '#FFFFFF',
-        'bg-dark-inner': '#282C2D',
-        'bg-dark-outer': '#3B3B3B',
-        'status-red': '#B03737',
-        'status-orange': '#B08E37',
-        'status-green': '#24FF00'
       },
       borderRadius: {
         'xl': '25px',

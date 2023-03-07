29160088819e46b296fe6c0aaa47322d49387bfa
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-07 20:51:01 +0100

    back to default

--- src/store/toastStore.ts
+++ src/store/toastStore.ts
@@ -7,18 +7,18 @@ interface toastStoreInterface {
     title: string,
     hideAfter?: number | undefined,
     icon?: string | undefined,
-    type?: ToastVariant,
+    type: ToastVariant,
     show: (text: string, title: string, type?: ToastVariant, hideAfter?: number) => void,
     hide: () => void,
 }
 
 export const useToastStore = create<toastStoreInterface>((set, get) => ({
-    visible: true,
-    text: 'texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext',
-    title: 'title',
+    visible: false,
+    text: '',
+    title: '',
     icon: undefined,
     hideAfter: undefined,
-    type: ToastVariant.FAILED,
+    type: ToastVariant.DEFAULT,
     show: (title: string, text: string, type?: ToastVariant, hideAfter?: number, icon?: string | undefined) => {
         set(state => ({ ...state, visible: true, text, title, hideAfter, icon, type }));
     },

dd2f491276c358d99864814e486a96579458cf5e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-12 20:54:49 +0100

    simplified status

--- src/atoms/Status.tsx
+++ src/atoms/Status.tsx
@@ -1,26 +1,19 @@
-import versions from '@config/Versions.json';
 import { UserStatus } from '@utils/enums';
+import { motion as m, MotionProps } from 'framer-motion';
+import { Ref, forwardRef } from 'react';
 
 type Props = {
     status: UserStatus,
 }
-type StatusCircleProps = {
-    color: string,
-}
 
 const STATUS_COLORS = [
-    'green',
-    'orange',
-    'red'
+    'bg-status-green',
+    'bg-status-orange',
+    'bg-status-red'
 ]
 
-const StatusCircle = ({ color }: StatusCircleProps) => {
-    const bgColor = 'bg-' + color;
-    return (<div className={`w-[9px] h-[9px] rounded ${bgColor}`}></div>);
-}
-
-const Status = ({ status }: Props) => {
-    return <StatusCircle color={STATUS_COLORS[status]} />
-}
+const Status = forwardRef(({ status }: Props, ref: Ref<HTMLDivElement>) => {
+    return (<m.div className={`w-[9px] h-[9px] rounded ${STATUS_COLORS[status]}`} ref={ref} />);
+});
 
 export default Status;

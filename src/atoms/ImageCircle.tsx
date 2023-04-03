1ccc4c850c4a99238e6b0f85cfc936210cfc7ccd
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-04-03 15:27:28 +0200

    Added string based size -> fix tailwind dynamic class shit

--- src/atoms/ImageCircle.tsx
+++ src/atoms/ImageCircle.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 type Props = {
     src: string,
-    size?: number | undefined,
+    size?: undefined | 'small' | 'medium' | 'large',
     hasBorder?: boolean
     disabled?: boolean,
     onClick?: Function | undefined
@@ -17,9 +17,14 @@ const ImageCircle = ({ src, size = undefined, hasBorder = false, disabled = fals
         { [`border-2 border-color-${userColor}`]: hasBorder }
     );
 
+    const circleSize = classNames(
+        { 'w-14 h-14 lg:w-16 lg:h-16': size === 'small' },
+        { 'w-16 h-16 lg:w-20 lg:h-20': size === undefined }
+    );
+
     return (
-        <div className={`inline-flex w-fit h-fit rounded-full justify-center items-center`}>
-            <img className={`image-circle rounded-full ${border}`} src={src} />
+        <div className={`inline-flex w-max h-max rounded-full justify-center items-center`}>
+            <img className={`image-circle rounded-full ${border} ${circleSize}`} src={src} />
         </div>
     )
 }

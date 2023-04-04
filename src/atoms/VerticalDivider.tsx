10738a0f70f13e5571c8ad43196c591410e152c7
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-04 22:50:24 +0200

    Added framer motion compatibility

--- src/atoms/VerticalDivider.tsx
+++ src/atoms/VerticalDivider.tsx
@@ -1,11 +1,14 @@
+import { forwardRef, Ref } from 'react';
 import { useUserSettings } from "@store/userSettings"
-
-const VerticalDivider = () => {
+interface VerticalDividerInterface {
+    className?: string;
+}
+const VerticalDivider = forwardRef(({ className }: VerticalDividerInterface, ref: Ref<HTMLDivElement>) => {
     const userColor = useUserSettings(state => state.color);
 
     return (
-        <div className={`vertical-divider bg-${userColor}`}></div>
+        <div ref={ref} className={`vertical-divider bg-${userColor} ${className}`}></div>
     )
-}
+})
 
-export default VerticalDivider
+export default VerticalDivider;

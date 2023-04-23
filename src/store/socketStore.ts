66d54a2dea42bd7b44ba33a2c41d506cd57dc02e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-23 21:47:59 +0200

    partner status, room extension

--- src/store/socketStore.ts
+++ src/store/socketStore.ts
@@ -1,24 +1,36 @@
+import { UserStatus } from '@utils/enums';
 import { create } from 'zustand';
 
 interface SocketStoreInterface {
     connected: boolean;
     connectedUsers: number;
     queuePopulation: number;
-    roomId: string | null;
+    roomId: roomIdInterface;
     partnerFound: boolean;
+    partnerStatus: UserStatus | undefined;
     setConnectedUsers: (num: number) => void;
     setConnected: (connectionState: boolean) => void;
     setQueuePopulation: (num: number) => void;
-    setRoom: (roomId: string | null) => void;
+    setRoom: (roomId: roomIdInterface) => void;
     setPartnerFound: (b: boolean) => void;
+    setPartnerstatus: (status: UserStatus) => void;
+}
+
+interface roomIdInterface {
+    current: string | null,
+    last: string | null
 }
 
 export const useSocketStore = create<SocketStoreInterface>((set, get) => ({
     connected: false,
     connectedUsers: 0,
     queuePopulation: 0,
-    roomId: null,
+    roomId: {
+        current: null,
+        last: null
+    },
     partnerFound: false,
+    partnerStatus: undefined,
 
     setConnectedUsers: (num: number) => {
         set(state => ({ ...state, connectedUsers: num }))
@@ -29,10 +41,13 @@ export const useSocketStore = create<SocketStoreInterface>((set, get) => ({
     setQueuePopulation: (num: number) => {
         set(state => ({ ...state, queuePopulation: num }))
     },
-    setRoom: (roomId: string | null) => {
+    setRoom: (roomId: roomIdInterface) => {
         set(state => ({ ...state, roomId }))
     },
     setPartnerFound: (b: boolean) => {
         set(state => ({ ...state, partnerFound: b }));
+    },
+    setPartnerstatus: (status: UserStatus) => {
+        set(state => ({ ...state, partnerStatus: status }));
     }
 }));

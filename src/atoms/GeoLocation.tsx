c37c0e19a0770a5a5e8c94ef1b33c8241bd3578f
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-04-30 17:54:58 +0200

    Separated location comp to use only the county string

--- src/atoms/GeoLocation.tsx
+++ src/atoms/GeoLocation.tsx
@@ -16,21 +16,31 @@ const GeoLocationWrapper = ({ children }: GeoLocationWrapper) => {
     return <div className={'text-cabin font-extralight text-sm text-center'}>{children}</div>;
 }
 
+export const UserCounty = (
+    { location, forceUnknown = false, bold = false }:
+        { location?: UserLocation, forceUnknown?: boolean, bold?: boolean }
+) => {
+    const wildCard = (<span className={`${bold ? 'font-semibold' : ''}`}>Ismeretlen</span>);
+    const countyName = counties.find(county => county.id === location)?.name || 'Hiányzó';
+    if (location === Location.UNKNOWN || location === Location.NOT_DEFINED) return wildCard;
+
+    return forceUnknown || location === undefined ? wildCard : <span className={`${bold ? 'font-semibold' : ''}`}>{countyName}</span>;
+}
+
 const GeoLocation = ({ permission, location }: GeoLocation) => {
-    const wildCard = (<GeoLocationWrapper> Jelenlegi tartózkodási helyed: <span className={'font-semibold'}>Ismeretlen</span></GeoLocationWrapper>);
     const loadingComponent = (<GeoLocationWrapper><LoadingIcon size={5} /></GeoLocationWrapper>);
-    if (location === Location.UNKNOWN) return wildCard;
+    //if (location === Location.UNKNOWN) return wildCard;
     if (location === Location.NOT_DEFINED) return loadingComponent;
 
-    const countyName = counties.find(county => county.id === location)?.name || 'Hiányzó';
+    //const countyName = counties.find(county => county.id === location)?.name || 'Hiányzó';
 
     switch (permission) {
         case GeoLocationPermission.PROMPT:
             return loadingComponent;
         case GeoLocationPermission.GRANTED:
-            return <GeoLocationWrapper>Jelenlegi tartózkodási helyed: <span className={'font-semibold'}>{countyName}</span> vármegye</GeoLocationWrapper>
+            return <GeoLocationWrapper>Jelenlegi tartózkodási helyed: <UserCounty location={location} bold={true} /> vármegye</GeoLocationWrapper>
         case GeoLocationPermission.DENIED:
-            return wildCard;
+            return <UserCounty forceUnknown={true} bold={true} />;
         default:
             return loadingComponent;
     }

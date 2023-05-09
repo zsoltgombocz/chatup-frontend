0a1559022e252651dc91fe2bbc7c57bf1d4fc0ce
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-05-09 19:16:49 +0200

    Fixing back button on socker error + header text

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -6,20 +6,42 @@ import { useAudio } from '@hooks/useAudio';
 import { config } from '@config/headerConfig';
 import { getURLSegment } from '@utils/url';
 import { useSocketStore } from '@/store/socketStore';
+import { socket } from '@/socket';
 
 type Props = {
     hideRouteText: boolean
 }
 
+const socketEffectedRoutes = [
+    'pre/interest',
+    'pre/gender',
+    'pre/location',
+    'pre',
+    'search',
+    'chat'
+];
+
 const NavigationHeader = ({ hideRouteText = false }: Props) => {
     const navigate = useNavigate();
     const sounds = useUserSettings(state => state.sounds);
     const { play } = useAudio();
+
+    const socketState = useSocketStore(state => state.connected);
+
+    const segment: string = getURLSegment(location.pathname, null) || '';
+
     const handleBackClick = () => {
         if (sounds) play('navigate');
+        console.log(!socketState, socketEffectedRoutes.includes(segment));
+        if (!socketState && socketEffectedRoutes.includes(segment)) {
+            console.log('nav??,')
+            navigate('/', { replace: true });
+        } else {
+            navigate(-1);
+        }
 
-        navigate(-1);
     }
+
     return (
         <div className={'flex flex-row items-center text text-2xl'}>
             <ChevronLeftIcon className="absolute left-10 h-7 w-7 cursor-pointer" onClick={handleBackClick} />
@@ -32,13 +54,12 @@ const NavigationTitle = () => {
     const location = useLocation();
     const [title, setTitle] = useState('');
     const socketState = useSocketStore(state => state.connected);
-    const customErrorHeader = ['pre', 'search', 'chat'];
 
     useEffect(() => {
         const segment: string = getURLSegment(location.pathname, null) || '';
         setTitle(config.routeNames[segment] ?? 'headerConfig: No entry');
 
-        if (!socketState && customErrorHeader.includes(segment)) setTitle('HIBA')
+        if (!socketState && socketEffectedRoutes.includes(segment)) setTitle('HIBA')
     }, [location, socketState]);
 
     return <>{title}</>;

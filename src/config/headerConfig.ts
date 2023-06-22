7b32737dc09c149e40fba8fbb2180fbe884be970
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-06-22 15:48:42 +0200

    Route names defined in linkedRoutes.tsx not here

--- src/config/headerConfig.ts
+++ src/config/headerConfig.ts
@@ -1,6 +1,5 @@
 interface HeaderConfigInterface {
     useNavigationRoutes: string[],
-    routeNames: { [id: string]: string },
     onlyBackButtonRoutes: string[],
     noBackground: string[],
     hideHeaderRoutes: string[]
@@ -11,18 +10,4 @@ export const config: HeaderConfigInterface = {
     onlyBackButtonRoutes: [],
     noBackground: ['pre'],
     hideHeaderRoutes: ['search', 'chat'],
-    routeNames: {
-        'devlogs': 'Verzióelőzmények',
-        'settings': 'Beállítások',
-        'settings/privacy': 'Adatvédelem',
-        'settings/customize': 'Testreszabás',
-        'settings/information': 'Névjegy',
-        'settings/help': 'Súgó',
-        'settings/contact': 'Kapcsolatfelvétel',
-        'devlog': 'Verzióelőzmények',
-        'pre': 'Válassz vármegyét',
-        'pre/location': 'Válassz vármegyét',
-        'pre/gender': 'Válaszd ki a nemed',
-        'pre/interest': 'Válassz ki 3 témát',
-    }
 }
